0d4ca1720dde1418d67f9436056546301175fcfb
Roger <thrasher_royerr666@hotmail.com>
2020-09-25 12:10:04 -0500

    First commit, chat app in real time done (lets chat)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
 
-function App() {
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCpjCsbgzwZGGzsQnbAJ8M8DRVpZgQwVwQ",
+  authDomain: "real-time-chat-e674d.firebaseapp.com",
+  databaseURL: "https://real-time-chat-e674d.firebaseio.com",
+  projectId: "real-time-chat-e674d",
+  storageBucket: "real-time-chat-e674d.appspot.com",
+  messagingSenderId: "300292290563",
+  appId: "1:300292290563:web:489c49cb799ddb4044b636",
+  measurementId: "G-1DMPYKNBRV",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+const App = () => {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          Real time chat <span role="img">💬</span>
+        </h1>
+        <SignOut />
       </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
-}
+};
+
+const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google{" "}
+        <img
+          src="https://icon-icons.com/icons2/729/PNG/128/google_icon-icons.com_62736.png"
+          alt=""
+        />
+      </button>
+      <p>Welcome</p>
+    </>
+  );
+};
+
+const SignOut = () => {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+};
+
+const ChatRoom = () => {
+  const dummy = useRef();
+
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Write something..."
+        />
+
+        <button type="submit" disabled={!formValue}>
+          <img
+            src="https://icon-icons.com/icons2/259/PNG/128/ic_send_128_28719.png"
+            alt=""
+          />
+        </button>
+      </form>
+    </>
+  );
+};
+
+const ChatMessage = (props) => {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+          alt=""
+        />
+        <p>{text}</p>
+      </div>
+    </>
+  );
+};
 
 export default App;
